261c393ae84695b0d1fb59f6c05be6cea04a184f
benoitantelme <benoitantelme@gmail.com>
2017-10-26 19:14:41 +0200

    Updated pom file to add dependencies Added words usage service and tests

---
+++ src/com/discord/bot/WordsUsageService.java
@@ -0,0 +1,36 @@
+package com.discord.bot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordsUsageService {
+
+    /**
+     * Map of words to map of author to count
+     */
+    protected final Map<String, Map<String, Integer>> wordsUsage = new HashMap<>();
+
+    protected String getWordsSummary() {
+        StringBuilder sb = new StringBuilder();
+
+        wordsUsage.keySet().forEach(key -> {
+            sb.append("For word ").append(key).append("\n");
+            wordsUsage.get(key).entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).
+                    forEach((e) -> sb.append("  ").append(e.getValue()).append(" times for author ").append(e.getKey()).append("\n"));
+        });
+
+        return sb.toString();
+    }
+
+    protected void updateWords(String word, String author) throws Exception {
+        if (wordsUsage.containsKey(word)) {
+            wordsUsage.get(word).merge(author, 1, (int1, int2) -> int1 + int2);
+        }else{
+            throw new Exception("Word " + word + " is not counted yet.");
+        }
+    }
+
+    protected void addWordIfMissing(String word) {
+        wordsUsage.put(word, new HashMap<>());
+    }
+}
---
+++ test/com/discord/bot/WordsUsageServiceTest.java
@@ -0,0 +1,54 @@
+package com.discord.bot;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class WordsUsageServiceTest {
+
+    public static final String APPLE = "apple";
+    public static final String TEST = "test";
+    public static final String TEST_2 = "test2";
+
+    @Test
+    public void testWordsMap() throws Exception {
+        WordsUsageService ms = new WordsUsageService();
+
+        ms.addWordIfMissing(APPLE);
+        ms.updateWords(APPLE, TEST);
+        ms.updateWords(APPLE, TEST_2);
+
+        try {
+            ms.updateWords("bird", TEST);
+        } catch (Exception e) {
+            assertNotNull(e);
+        }
+
+        assertEquals(1, ms.wordsUsage.entrySet().size());
+
+        Map<String, Integer> apples = ms.wordsUsage.get(APPLE);
+        assertEquals(2, apples.entrySet().size());
+        assertTrue(apples.get(TEST).equals(1));
+    }
+
+    @Test
+    public void testWordsSummary() throws Exception {
+        WordsUsageService ms = new WordsUsageService();
+
+        String result = ms.getWordsSummary();
+        assertTrue(result.isEmpty());
+
+        ms.addWordIfMissing(APPLE);
+        result = ms.getWordsSummary();
+        assertFalse(result.isEmpty());
+
+        ms.updateWords(APPLE, TEST);
+        ms.updateWords(APPLE, TEST_2);
+        result = ms.getWordsSummary();
+        assertFalse(result.isEmpty());
+    }
+
+
+}
