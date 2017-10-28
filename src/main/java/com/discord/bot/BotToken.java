acd247aefb190f327d7881ea303c6585b2384d4e
benoitantelme <benoitantelme@gmail.com>
2017-10-28 22:43:33 +0200

    Added message listener bot

---
+++ src/main/java/com/discord/bot/BotToken.java
@@ -0,0 +1,7 @@
+package com.discord.bot;
+
+public class BotToken {
+
+    public static final String TOKEN = "your token here";
+
+}
---
+++ src/main/java/com/discord/bot/WordsBot.java
@@ -0,0 +1,39 @@
+package com.discord.bot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+public class WordsBot extends ListenerAdapter {
+
+    public static void main(String[] args)
+            throws LoginException, RateLimitedException, InterruptedException {
+
+        WordsUsageService ms = new WordsUsageService();
+
+        ms.addWordIfMissing("apple");
+
+        JDA jda = new JDABuilder(AccountType.BOT).setToken(BotToken.TOKEN).buildBlocking();
+        jda.addEventListener(new WordsBot());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot())
+            return;
+
+        Message message = event.getMessage();
+        String msg = message.getContent();
+
+        MessageChannel channel = event.getChannel();
+        channel.sendMessage(msg).queue();
+    }
+
+}
